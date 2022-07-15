b115bca890fc014a2f421534d4ab87087c295489
FOXYSAMU <39921257+FOXYSAMU@users.noreply.github.com>
2022-07-15 18:02:13 -0500

    Animations working perfectly

--- app.js
+++ app.js
@@ -20,66 +20,109 @@ const Players = (name) => {
             player2Name.textContent = player2.getName();
             player2Name.classList.remove('empty');
         }
-    }
-    return {setNames, getName}
+    };
+
+    return { setNames, getName }
 }
+
+const gameScore = (() => {
+    const player1Score = document.querySelector('#player1Score p');
+    const player2Score = document.querySelector('#player2Score p');
+
+    const restartRoundsBtn = document.getElementById('restartBtn');
+    
+    function addUpScore(playerScore) {
+        if(playerScore.name === 'player1') {
+            ++playerScore.score;
+            player1Score.textContent = playerScore.score;
+        } else if(playerScore.name === 'player2') {
+            ++playerScore.score;
+            player2Score.textContent = playerScore.score;
+        }
+
+        restartRoundsBtn.addEventListener('click', restartRounds);
+
+        function restartRounds() {
+            console.log(playerScore)
+            playerScore.score = 0;
+            player1Score.textContent = playerScore.score;
+            player2Score.textContent = playerScore.score;
+            console.log(playerScore)
+        };
+    };
+
+    return { addUpScore }
+})();
     
 const gameBoard = (() => {
     let board = ['', '', '', '', '', '', '', '', ''];
     const X_Mark = 'X';
     const O_Mark = 'O';
-    
-    // let canPlay = false;
+    let playerScores = [
+        {
+            'name' : 'player1',
+            'score' : 0
+        },
+        {
+            'name' : 'player2',
+            'score' : 0
+        }
+    ];
     let circTurn;
     const cells = document.querySelectorAll('[data-cell]');
-    const restartBtn = document.getElementById('restartBtn');
 
-    playBefore()
+    const drawCont = document.querySelector('.draw-txt');
+    const drawTxt = document.querySelector('.draw-p');
+
+    playBefore();
 
     function playBefore() {
         board = ['', '', '', '', '', '', '', '', ''];
         circTurn = false;
         cells.forEach((cell, i) => {
             cell.textContent = '';
-            cell.addEventListener('click', (e) => {
+            cell.addEventListener('click', cellClick)
+            function cellClick(e) {
                 const cellIndex = i;
                 const currCell = e.target;
                 const currPlay = circTurn ? O_Mark : X_Mark;
                 if(board[cellIndex] === "") {
                     placeMarks(currCell, currPlay, cellIndex);
-                    changeTurns();
-                    currentTurn();
                 }
-            });
+            };
         }) 
         currentTurn();
-    }
+    };
 
     function placeMarks(cell, play, index) {
         board[index] = play;
-        console.log(board)
         cell.textContent = play;
         if(checkForWin(play)) {
-            console.log('winner')
+            finishGame(false);
+        } else if(isDraw()) {
+            finishGame(true);
+        } else {
+            changeTurns();
+            currentTurn();
         }
-    }
+    };
 
     function changeTurns() {
         circTurn = !circTurn;
-    }
+    };
     
     function currentTurn() {
         const player1Name = document.querySelector('.player1Name');
         const player2Name = document.querySelector('.player2Name');
        
         if(circTurn) {
-            player2Name.classList.add('current');
-            player1Name.classList.remove('current');
+            addGlow(player2Name);
+            removeGlow(player1Name);
         } else {
-            player1Name.classList.add('current');
-            player2Name.classList.remove('current');
+            addGlow(player1Name);
+            removeGlow(player2Name);
         }
-    }
+    };
 
     const checkForWin = (play) => {
         const checkWinCombinations = [
@@ -95,25 +138,83 @@ const gameBoard = (() => {
 
         return checkWinCombinations.some(combination => {
             return combination.every(i => {
-                return cells[i].textContent.includes(play);
+                return board[i].includes(play);
             })
         })
     };
 
-    // const finishGame = () => {
-    //     if (isDraw) {
+    function isDraw() {
+        return board.every(item => {
+            return item.includes(X_Mark) || item.includes(O_Mark)
+        })
+    };
 
-    //     } else {
-    //         if(circTurn ? :)
-    //     }
-    // }
+    const { addUpScore } = gameScore;
 
-    return { playBefore }
+    const finishGame = (Draw) => {
+        if(Draw) {
+            drawAnimation();
+            cellClear();
+        } else {    
+            if(circTurn)  {
+                cellClear();
+                addUpScore(playerScores[1]);
+            }
+            else {
+                cellClear();
+                addUpScore(playerScores[0]);
+            }
+        }
+    };
+
+    function drawAnimation() {
+        drawCont.classList.add('longfadein');
+        drawTxt.textContent = "It's a draw!";
+        drawCont.addEventListener('animationend', (e) => {
+            e.stopPropagation();
+            drawCont.classList.remove('longfadein');
+            drawTxt.classList.add('longfade')
+            drawTxt.addEventListener('animationend', (e) => {
+                e.stopPropagation();
+                drawTxt.classList.remove('longfade');
+                drawTxt.textContent = '';
+            })
+        })
+    }
+
+    function cellClear() {
+        board = ['', '', '', '', '', '', '', '', ''];
+        cells.forEach(cell => {
+            cell.textContent = '';
+            cell.classList.add('fade');
+            cell.addEventListener('animationend', (e) => {
+                e.stopPropagation();
+                cell.classList.remove('fade');
+                cell.classList.add('fadein');
+                cell.addEventListener('animationend', () => {
+                    cell.classList.remove('fadein');
+                })
+            })
+        })
+    };
+
+    function addGlow(el) {
+        el.classList.add('glow');
+    }
+
+    function removeGlow(el) {
+        el.classList.remove('glow');
+    }
+
 })();
 
 const changeBetweenPages = (() => {
     const optionsCont = document.querySelector('.options-cont');
     const mainBoard = document.querySelector('.main-board');
+
+    const animation1 = document.querySelector('.foranimation-1')
+
+    const animation2 =  document.querySelector('.foranimation-2');
 
     const player1Input = document.querySelector('input[placeholder="Player 1"]');
     const player2Input = document.querySelector('input[placeholder="Player 2"]');
@@ -121,42 +222,51 @@ const changeBetweenPages = (() => {
     const startBtn = document.getElementById('startBtn');
     const gobackBtn = document.getElementById('gobackBtn');
 
-    startBtn.addEventListener('click', changePageForward);
-    gobackBtn.addEventListener('click', changePageBackwards);
+    startBtn.addEventListener('click', () => {
+        getPlayerInput();
+        player1Input.value = '';
+        player2Input.value = '';
+        addFade(optionsCont);
+        optionsCont.addEventListener('animationend', (e) => {
+            optionsCont.classList.add('hidden');
+            mainBoard.classList.remove('hidden');
+            removeFade(optionsCont);
+            animation2.classList.add('fadeIndisp');
+            animation2.addEventListener('animationend', (e) => {
+                e.stopPropagation();
+                animation2.classList.remove('fadeIndisp');
+            });
+        });
+    });
 
-    function changePageForward() {
-        if (!optionsCont.classList.contains('hidden')) {
-            getPlayerInput()
-            player1Input.value = '';
-            player2Input.value = '';
-            optionsCont.classList.add('fade');
-            optionsCont.addEventListener('animationend', () => {
-                optionsCont.classList.add('hidden');
-                mainBoard.classList.remove('hidden');
-                optionsCont.classList.remove('fade')
-            })
-        }
-    }
+    gobackBtn.addEventListener('click', () => { 
+        addFade(mainBoard);
+        mainBoard.addEventListener('animationend', (e) => {
+            optionsCont.classList.remove('hidden');
+            mainBoard.classList.add('hidden');
+            removeFade(mainBoard);
+            animation1.classList.add('fadeIndisp');
+            animation1.addEventListener('animationend', (e) => {
+                e.stopPropagation();
+                animation1.classList.remove('fadeIndisp');
+            });
+        });
+    });
 
-    const {playBefore} = gameBoard;
+    function addFade(el) {
+        el.classList.add('fade');
+    };
 
-    function changePageBackwards() {
-        if (!mainBoard.classList.contains('hidden')) {
-            mainBoard.classList.add('fade');
-            mainBoard.addEventListener('animationend', () => {
-                playBefore()
-                optionsCont.classList.remove('hidden');
-                mainBoard.classList.add('hidden');
-                mainBoard.classList.remove('fade');
-            })
-        }
-    }
-
+    function removeFade(el) {
+        el.classList.remove('fade');
+    };
+    
     function getPlayerInput() {
         const player1 = Players(player1Input.value);
         const player2 = Players(player2Input.value);
         player1.setNames(player2);
-    }
+    };
+
 })();
 
 
